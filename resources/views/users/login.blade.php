e603cb7e6a30e251f28c590e8c6e46785dd771e5
mohsin155 <mohsinhassan155@gmail.com>
2017-04-13 17:37:49 +0530

    login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -14,12 +14,22 @@
             <div class="col-md-10 link_color"><a href="#" class="link_color">Home</a> | <a href="{{url('/signup')}}" >Join Free Now</a> | <a href="#">Testinomials</a></div>
             <div class="col-md-2 text-right pd-0"><!--<img src="image/flag_English.gif" /><img src="image/arrowdown.png" class="pd-l-10" />--> </div>
         </div>
+        
         <form  name="login-page" id="login-page" method="post" action="{{url('users/login')}}">
             <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
             <div class="col-md-12 main_content pd-b-70">
                 <div class="col-md-12 rounded_container rounder_continer_outer mr-t-20">
                     <div class="col-md-5 login_box  mr-t-30 mr-b-40 pd-0">
                         <div class="text-center greygrad heading">Members Login</div>
+                        @if(!empty($errors) && count($errors)>0)
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach($errors as $messages)
+                                        <li> {{$messages}} </li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                                @endif
                         <div class="pd-l-20 pd-r-20 pd-t-10">
                             <ul class="fberror errors hide"></ul>
                             <input name="page" value="" type="hidden">
@@ -45,6 +55,7 @@
                             </div>
 
                         </div>
+                         
                         <div class="horizontalLine pd-t-5">
                             <span class="orText">OR</span>
                         </div>
