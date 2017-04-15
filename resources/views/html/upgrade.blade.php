d4cb52eec1126566b1e1e2246833d725fe8cc02b
sagarkapoor5830 <sagar.kapoor@appster.in>
2017-04-15 11:31:24 +0530

    dating site progress

--- resources/views/html/upgrade.blade.php
+++ resources/views/html/upgrade.blade.php
@@ -1,6 +1,37 @@
 @extends('layouts.main')
 @section('content')
 <div class="content-wrapper-inner">
-   Upgrade
+    <div class="content-wrapper-settings">
+     <form name="" class="form-inline" id="" novalidate="novalidate">
+         <div class="upgrade-top-section">
+        <div class="steps">
+		<span class="step">
+			<div class="circle">3</div>
+			<div class="vertical-table">
+				<p>Receipt</p>
+			</div>
+		</span>
+		<div class="rightarrow"></div>
+		<span class="step">
+			<div class="circle">2</div>
+			<div class="vertical-table">
+				<p>Personal Details</p>
+			</div>
+		</span>
+		<div class="rightarrow"></div>
+		<span class="step">
+			<div class="circle selected">1</div>
+			<div class="vertical-table">
+				<p>Choose Membership</p>
+			</div>
+		</span>
+	</div>
+
+    </div>
+         <div class="col-md-12 banner-section pd-t-20">
+             <img src="../image/banner.png" alt="banner image" width="770">
+         </div>
+             </form>
+    </div>
 </div>
 @endsection
