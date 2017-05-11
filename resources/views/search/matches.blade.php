652348b17e4624d54a5378e57a608a2c0c437696
Mohsin Hassan <mohsinhassan@Mohsin-hassan.local>
2017-05-11 20:40:38 +0530

    bugs

---
+++ resources/views/search/matches.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.main')
+@section('content')
+<div id="bannerwrapper" class="clearfix" style="margin-top: 0px;">
+    <div id="bannerpage">
+        <div id="main-content">
+            <div class="searchhdg">
+                <div class="searchhdg2">
+                    <h1> My Matches
+                        <img src="{{asset('image/q-mark.gif')}}" title="Users on this page match the criteria you specified in your match settings. You can edit your match settings by clicking the 'Improve Matches' link." class="tipMe">
+                    </h1>
+
+                    <a href="/match_registration.cfm">Improve Matches</a>
+                </div>
+                <div class="matchnav">
+
+
+                    <div class="matchnav_matches">
+                        <span class="matches selectedTab"><img src="{{asset('image/icon_mymatches.png')}}" class="matchicon">My Matches</span> <a href="#"><img border="0" src="{{asset('image/icon_mutualmatches.png')}}" class="matchicon">Mutual Matches</a> <a href="#" class="reverse"><img border="0" src="{{asset('image/icon_reversematches.png')}}" class="matchicon">Reverse Matches</a>
+                    </div>
+
+                    <div class="clear"></div>
+
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="memberships">
+                @foreach($matches as $row)
+                 <div class="standardview">
+                    <div class="standardinfo" style="">
+
+                        <span class="memberpic">
+
+                            <a href="javascript:void(0)" class="no-photo-display-popup">
+
+                                <img border="0" height="136px" width="125px" src="{{url('uploads')}}/{{Auth::user()->user_id}}{{$row->photo_name}}">
+
+                            </a>
+
+                        </span>
+                        <div class="text">
+
+                            <p class="hdg1">
+
+                                <a href="#">Gessica (24)</a>
+
+                            </p>
+
+                            <p><span class="hdg2"></span>
+                                Ribeirão Prêto,
+
+                                São Paulo, Brazil
+                                <br>
+                                <strong>Seeking:</strong> Male 25 - 42<br>
+
+                                <strong>Last Login:</strong> 0 min ago</p>
+                        </div>
+
+                        <span class="icons">
+                            <ul class="iconset">
+                                <li class="iconstandard">
+                                    <a href="#" title="click here to view profile"></a>
+                                </li>
+                                <li class="iconmail">
+                                    <a style="display:none;" data-sitetranslationpath="en" class="launchRegistrationModal" href="javascript: void(0);"></a>
+                                    <a href="#" title="Send Gessica a message" class="emailpopup" rel="8757906" memname="Gessica"></a>
+                                </li>
+                                <li class="iconinterest sendinterest" name="070086E1CDC940B4459D0A" data-altclass="iconinterestsent" data-msid="8757906" data-imageurl="" data-name="Gessica"><a href="/en/memberrelationship/showinterest/070086E1CDC940B4529D0A" title="Show interest in Gessica"></a></li>
+                                <li class="iconfavorites addfavorites" name="070086E1CDC940B4459D0A" data-altclass="iconfavoritessent" data-imageurl="" data-name="Gessica"><a href="/en/memberrelationship/addfavorite/070086E1CDC940B4529D0A" title="Add Gessica to your favorites"></a></li>
+                                <li class="icononlinemobileactive">
+                                    <a href="#" title="I'm Online - click to chat"></a>
+                                </li>
+                            </ul>
+                        </span>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
